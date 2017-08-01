b7454ae1f77e26c2be7b458e83c5f284ca505b07
Tre Murillo <treelzebub@users.noreply.github.com>
2017-07-31 22:59:47 -0700

    initial commit

---
+++ core/src/main/kotlin/net/treelzebub/kanalyzer/core/Kanalyzer.kt
@@ -0,0 +1,8 @@
+package net.treelzebub.kanalyzer.core
+
+class Kanalyzer {
+
+    fun init() {
+
+    }
+}
---
+++ core/src/main/kotlin/net/treelzebub/kanalyzer/main.kt
@@ -0,0 +1,8 @@
+package net.treelzebub.kanalyzer
+
+import net.treelzebub.kanalyzer.core.Kanalyzer
+
+fun main(vararg args: String) {
+    val kanalyzer = Kanalyzer()
+    kanalyzer.init()
+}
